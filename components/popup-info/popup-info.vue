87760782ed664885a6dbb7660a753d76a0aba658
JunHaoX <451696741@qq.com>
2022-11-06 23:52:57 +0800

    上传文章

---
+++ components/popup-info/popup-info.vue
@@ -0,0 +1,165 @@
+<template>
+		<uni-popup ref="popup" type="bottom" :mask-click="false">
+				<view class="release-detailContainer">
+					<!-- 关闭按钮 -->
+					<view class="close_btn" @click="changeShow">
+						<uni-icons type="closeempty"></uni-icons>
+					</view>
+					<!-- 请添加内容描述描述 -->
+					<view class="content-commary">
+						<text class="content-commary_text">简要描述:</text>
+						<textarea v-model="value" placeholder="请输入内容" maxlength="35">
+						</textarea>
+						<view class="commary-length">
+							{{value.length}}/35
+						</view>
+					</view>
+				
+					<!-- 选择文章模式 -->
+					<view class="content-mode">
+						<text class="content-commary_text">文章模式:</text>
+						<view class="mode-box">
+							 <u-radio-group
+							     v-model="radioVal"
+							     placement="row"
+							     @change="groupChange"
+							   >
+							     <u-radio
+							       :customStyle="{marginLeft: '10px'}"
+							       v-for="(item, index) in radioList"
+							       :key="index"
+							       :label="item.label"
+							       :name="item.name"
+							     >
+							     </u-radio>
+							   </u-radio-group>
+
+						</view>
+						
+					</view>
+				</view>
+		</uni-popup>
+</template>
+
+<script>
+	export default {
+		props:{
+			isShow:{
+				type:Boolean,
+				default:false
+			}
+		},
+		watch:{
+			isShow(newVal){
+				if(newVal){
+					this.open()
+				}else{
+					this.close()
+				}
+				
+			}
+		},
+		data() {
+			return {
+				//简要标题
+				value:'',
+				//单选
+				radioVal:'base',
+				radioList:[
+					{
+						id:1,
+						name:'base',
+						label:'基本排版'
+					},
+					{
+						id:2,
+						name:'image',
+						label:'大图排版'
+					},
+					{
+						id:3,
+						name:'column',
+						label:'多图排版'
+					},
+					
+				]
+				
+			};
+		},
+		methods: {
+			 open(){
+			        // 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+			        this.$refs.popup.open('center')
+					},
+			close(){
+				this.$refs.popup.close()
+			},
+			changeShow(){
+				this.$emit('changeShow')
+			},
+			groupChange(name){
+				console.log(name)
+			}
+	},
+	}
+</script>
+
+<style lang="scss" scoped>
+.release-detailContainer{
+			width: 720rpx;
+			height: 1000rpx;
+			background-color: $uni-bg-color-grey;
+			border-radius: $uni-border-radius-lg;
+			box-sizing: border-box;
+			padding: $uni-spacing-row-lg;
+			position: relative;
+			.close_btn{
+				position: absolute;
+				right: 0;
+				top: 0;
+				padding: $uni-spacing-col-base;
+			}
+			.content-commary{
+				margin-top: $uni-spacing-row-base;
+				position: relative;
+				textarea{
+					width: 100%;
+					font-size:$uni-font-size-lg;
+					height: 70px;
+					margin: 10px auto;
+					color:$uni-text-color-title;
+					box-sizing: border-box;
+					padding: $uni-spacing-row-sm;
+					border: 1px solid #c8c7cc;
+					box-shadow: $uni-boxShadow-base;
+					border-radius:  $uni-border-radius-lg;
+				}
+				.commary-length{
+					color: $uni-text-color-grey;
+					position: absolute;
+					right: 5px;
+					bottom: 5px;
+				}
+			}
+			.content-mode{
+				.mode-box{
+					display: flex;
+					justify-content: space-between;
+					margin: $uni-spacing-row-base 0;
+					box-sizing: border-box;
+					.mode-base{
+						width: 30%;
+						height: 100px;
+						border: 1px solid $uni-border-color;
+						
+					}
+				}
+				
+			}
+}
+.content-commary_text{
+					font-weight: bold;
+					letter-spacing: 1mm;
+				}
+
+</style>
